cbbe1f95a1216122ecba0917115e004a3aff863d
Sergey Schegolev <sergey.schegolev@pandadoc.com>
2021-10-02 11:11:29 +0300

    Fix review comments

--- Lesson2/src/main/java/Lesson2.java
+++ Lesson2/src/main/java/Lesson2.java
@@ -7,13 +7,15 @@ public class Lesson2 {
         System.out.println(max(56, 349));
         System.out.println(average(new int[]{0, -2, 3, -1, 5}));
         System.out.println(max(new int[]{1, 2, 3, 4, 5, 100, 99}));
+        System.out.println(max(null));
         System.out.println(calculateHypotenuse(3,4));
     }
 
     public static int sum(int a, int b) {
-        int result = a + b;
-        // просто больше никогда не получится, т.к. при сложении получается переполнение
-        return result >= Integer.MAX_VALUE ? 1 : result;
+        long longResult = (long)a + b;
+        int convertedIntResult = (int)longResult;
+
+        return convertedIntResult != longResult ? -1 : convertedIntResult;
     }
 
     public static double max(int a, int b) {
@@ -21,11 +23,23 @@ public class Lesson2 {
     }
 
     public static double average(int[] array) {
-        return Arrays.stream(array).average().orElse(Double.NaN);
+        if (array == null) {
+            return 0;
+        }
+
+        return Arrays.stream(array)
+                .average()
+                .orElse(0);
     }
 
     public static int max(int[] array) {
-        return Arrays.stream(array).max().orElse(0);
+        if (array == null) {
+            return 0;
+        }
+
+        return Arrays.stream(array)
+                .max()
+                .orElse(0);
     }
 
     public static double calculateHypotenuse(int a, int b) {
