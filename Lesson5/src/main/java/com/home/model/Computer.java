96655a6b13dc6ff8d2153460daa21c4ad4a56456
Sergey Schegolev <sergey.schegolev@pandadoc.com>
2021-10-15 12:50:05 +0300

    Fix comments

--- Lesson5/src/main/java/com/home/model/Computer.java
+++ Lesson5/src/main/java/com/home/model/Computer.java
@@ -5,23 +5,56 @@ import java.util.Scanner;
 
 public class Computer {
 
-    String processor;
-    int memory;
-    int hardDisk;
-    int resource;
+    private String processor;
+    private int memory;
+    private int hardDisk;
+    private int resource;
 
-    public Computer(String proc, int memory, int hardDisk, int resource) {
-        this.processor = proc;
+    public Computer(String processor, int memory, int hardDisk, int resource) {
+        this.processor = processor;
         this.memory = memory;
         this.hardDisk = hardDisk;
         this.resource = resource;
     }
 
     public void describeComputer() {
-        System.out.println("Processor: " + this.processor);
-        System.out.println("Memory: " + this.memory);
-        System.out.println("Hard disk: " + this.hardDisk);
-        System.out.println("Resources: " + this.resource);
+        System.out.println("Processor: " + processor);
+        System.out.println("Memory: " + memory);
+        System.out.println("Hard disk: " + hardDisk);
+        System.out.println("Resources: " + resource);
+    }
+
+    public void on() {
+        if (resource == 0) {
+            System.out.println("Computer was crushed");
+        } else {
+            boolean wasOperationSuccessful =
+                    getRandomOperationResult("Please, enter 0 or 1 to turn on the computer: ");
+
+            if (wasOperationSuccessful) {
+                System.out.println("Computer is on");
+                off();
+            } else {
+                System.out.println("Computer was crushed");
+            }
+        }
+    }
+
+    public void off() {
+        if (resource == 0) {
+            System.out.println("Computer was crushed");
+        } else {
+            resource--;
+            boolean wasOperationSuccessful =
+                    this.getRandomOperationResult("Please, enter 0 or 1 to turn off the computer: ");
+
+            if (wasOperationSuccessful) {
+                System.out.println("Computer is off");
+                on();
+            } else {
+                System.out.println("Computer was crushed");
+            }
+        }
     }
 
     private boolean getRandomOperationResult(String operationQuestion) {
@@ -33,39 +66,5 @@ public class Computer {
         int enteredNumber = scanner.nextInt();
 
         return randomInt == enteredNumber;
-    }
-
-    public void on() {
-        if (this.resource == 0) {
-            System.out.println("Computer was crushed");
-            return;
-        }
-
-        boolean wasOperationSuccessful =
-                this.getRandomOperationResult("Please, enter 0 or 1 to turn on the computer: ");
-
-        if (wasOperationSuccessful) {
-            System.out.println("Computer is on");
-            this.off();
-        } else {
-            System.out.println("Computer was crushed");
-        }
-    }
-
-    public void off() {
-        if (this.resource == 0) {
-            System.out.println("Computer was crushed");
-            return;
-        }
-
-        this.resource--;
-        boolean wasOperationSuccessful =
-                this.getRandomOperationResult("Please, enter 0 or 1 to turn off the computer: ");
-
-        if (wasOperationSuccessful) {
-            System.out.println("Computer is off");
-        } else {
-            System.out.println("Computer was crushed");
-        }
     }
 }
